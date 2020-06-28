5b963dd858f93280e556237768e84d45d6ff58f1
Sacha Sedriks <sachased@gmail.com>
2020-06-28 14:46:31 -0700

    init boilerplate node with mocha test framework and chai assertion library.

---
+++ README.md
@@ -0,0 +1,5 @@
+## node testing with mocha and chai
+
+per: https://courses.thinkful.com/node-postgres-v1/checkpoint/5
+
+**[Mocha](https://mochajs.org/)** test framework and **[Chai](https://www.chaijs.com/guide/styles/#expect)** assertion library (expect style).
---
+++ test/divide.test.js
@@ -0,0 +1,39 @@
+const divide = require('../index');
+const expect = require('chai').expect;
+
+describe('Divide function', () => {
+
+  it('should divide positive integers correctly', () => {
+    
+    // DEFINE inputs
+    const a = 8, b = 4;
+    // EXPECT
+    const expectedAnswer = 2;
+
+    // ACTUAL invoke function
+    const acutalAnswer = divide(a, b)
+
+    // ASSERT that expected === actual
+    expect(acutalAnswer).to.equal(expectedAnswer);
+    // ^^^^^^^
+    // using CHAI assertion library 'expect' syntax
+    // https://www.chaijs.com/guide/styles/#expect
+
+  });
+
+  it('should throw an error when divide by zero', () => {
+   
+    // define inputs
+    const a = 8, b = 0;
+    
+    // set up the function call
+    // to prevent interuption of normal execution flow when an exception is thrown, we wrap the invocation of our function divide() in another function
+    const fn = () => {
+      divide(a, b)
+    }
+    
+    // assert that exception is thrown
+    expect(fn).to.throw();
+
+  });
+});
